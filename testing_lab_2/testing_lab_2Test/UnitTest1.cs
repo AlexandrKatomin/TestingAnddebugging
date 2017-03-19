4ac3580ab45c3de6c74da6fc68a7cb8970734edf
AlexandrKatomin <san-bel-raz@yandex.ru>
2017-03-19 21:05:42 +0300

    add test white box

--- testing_lab_2/testing_lab_2Test/UnitTest1.cs
+++ testing_lab_2/testing_lab_2Test/UnitTest1.cs
@@ -9,7 +9,6 @@ namespace testing_lab_2.Tests {
         public void TestOfEmptyArray() {
             List<int> array = new List<int>();
             List<int> expected = new List<int>();
-
             List<int> actual = Sort.SortByChoice(array);
             CollectionAssert.AreEqual(expected, actual);
         }
@@ -24,15 +23,62 @@ namespace testing_lab_2.Tests {
         public void TestOfEvenArray() {
             List<int> array = new List<int>() { 7, 4, 1, 6, 8, 2 };
             List<int> expected = new List<int>() { 1, 2, 4, 6, 7, 8 };
+            List<int> actual = Sort.SortByChoice(array);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        public void TestOfOddArray() {
+            List<int> array = new List<int>() { 7, 4, 1, 6, 8, 2, 5 };
+            List<int> expected = new List<int>() { 1, 2, 4, 5, 6, 7, 8 };
+            List<int> actual = Sort.SortByChoice(array);
+            CollectionAssert.AreEqual(expected, actual);
+        }
 
+        [TestMethod]
+        public void TestOfSortedArray() {
+            List<int> array = new List<int>() { 1, 2, 3, 4, 5 };
+            List<int> expected = new List<int> { 1, 2, 3, 4, 5 };
+            List<int> actual = Sort.SortByChoice(array);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+
+        //-------------------------------------------------------------------------------------
+        //тестирование белого ящика 
+
+
+
+       
+        [TestMethod]
+        //пустой массив
+        public void TestofArea_1() {
+            List<int> array = new List<int>();
+            List<int> expected = new List<int>();
+            List<int> actual = Sort.SortByChoice(array);
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        //массив из 1 элемента
+        public void TestofArea_2() {
+            List<int> array = new List<int>() { 1 };
+            List<int> expected = new List<int>() { 1 };
             List<int> actual = Sort.SortByChoice(array);
             CollectionAssert.AreEqual(expected, actual);
         }
         [TestMethod]
-        public void TestOfOddArray() {
-            List<int> array = new List<int>() { 7, 4, 1, 6, 8, 2,5 };
-            List<int> expected = new List<int>() { 1, 2, 4,5, 6, 7, 8 };
+        // отсортированный массив
+        public void TestofArea_3() {
+            List<int> array = new List<int> { 1, 3, 2 };
+            List<int> expected = new List<int> { 1, 2, 3 };
+            List<int> actual = Sort.SortByChoice(array);
+            CollectionAssert.AreEqual(expected, actual);
+        }
 
+        [TestMethod]
+        // неотсортированный массив
+        public void TestofArea_4() {
+            List<int> array = new List<int>() { 7, 4, 1, 6, 8, 2, 5 };
+            List<int> expected = new List<int>() { 1, 2, 4, 5, 6, 7, 8 };
             List<int> actual = Sort.SortByChoice(array);
             CollectionAssert.AreEqual(expected, actual);
         }
