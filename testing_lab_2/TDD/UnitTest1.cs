0e374b366e85436bdd9f35da8446ae91397f0c5c
AlexandrKatomin <san-bel-raz@yandex.ru>
2017-03-20 22:02:47 +0300

    create testInsert for 2 and 3 elements

--- testing_lab_2/TDD/UnitTest1.cs
+++ testing_lab_2/TDD/UnitTest1.cs
@@ -10,17 +10,28 @@ namespace testing_lab_2.TDD {
             BinaryTree tree = new BinaryTree();
             //Assert.IsInstanceOfType(tree, BinaryTree);  ? ошибка не компилируется
         }
-        [TestMethod]
-        public void CreateTree() {
-            BinaryTree tree = new BinaryTree();
-            //Assert.IsInstanceOfType(tree, BinaryTree);  ? ошибка не компилируется
-        }
-
 
         [TestMethod]
         public void InsertElement() {
             BinaryTree tree = new BinaryTree();
-          //  tree.Add(5);
+            tree.Add(5);
+            Assert.AreEqual(tree.Data, 5);
+        }
+        public void InsertTwoElement() {
+            BinaryTree tree = new BinaryTree();
+            tree.Add(5);
+            tree.Add(8);
+            Assert.AreEqual(tree.Data, 5);
+            Assert.AreEqual(tree.Right.Data, 8);
+        }
+        public void InsertThreeElement() {
+            BinaryTree tree = new BinaryTree();
+            tree.Add(5);
+            tree.Add(8);
+            tree.Add(7);
+            Assert.AreEqual(tree.Data, 5);
+            Assert.AreEqual(tree.Right.Data, 8);
+            Assert.AreEqual(tree.Right.Left.Data, 8);
         }
     }
 }
