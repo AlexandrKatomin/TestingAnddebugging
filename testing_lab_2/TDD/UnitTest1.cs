1ad0aa388241d776b65d9d6d1335e9949957796a
AlexandrKatomin <san-bel-raz@yandex.ru>
2017-03-20 14:23:41 +0300

    first test tdd

---
+++ testing_lab_2/TDD/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDD {
+    [TestClass]
+    public class TestOfBinaryTree {
+        [TestMethod]
+        public void CreateTree() {
+            BinaryTree tree = new BinaryTree();
+        }
+    }
+}
--- testing_lab_2/testing_lab_2/Program.cs
+++ testing_lab_2/testing_lab_2/Program.cs
@@ -8,9 +8,7 @@ namespace testing_lab_2 {
     class Program {
       
         static void Main(string[] args) {
-            List<int> array = new List<int>() { 7, 4, 1, 6, 8, 2 };
-            List<int> array2 = Sort.SortByChoice(array);
-            int t = 66;
+
 
 
         }
